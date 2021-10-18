aed1bd7abdba2eb035a3c4a76b2657b4073ffd17
kubusq233 <92712561+kubusq233@users.noreply.github.com>
2021-10-18 12:55:14 +0200

    Add files via upload

---
+++ b.php
@@ -0,0 +1,38 @@
+<form action="b.php" method="POST">
+Login: <input type="text" name="login"/> <br/>
+Haslo: <input type="password" name="haslo"/> <br/>
+<button type="submit">zaloguj</button>
+</form>
+
+<?php 
+$con = mysqli_connect("localhost", "root","","logowanie"); //laczenie z baza
+
+$login = $_POST['login']; //pobieranie loginu
+$haslo = $_POST['haslo']; //pobieranie hasla
+
+$zap = "SELECT * FROM `loguj`"; //zapytanie wybierajace wszystkie pola z tabeli loguj
+$wyn = mysqli_query($con, $zap); 
+while($row = mysqli_fetch_array($wyn))
+{
+	$has = $row['haslo'];
+	$log = $row['login'];
+	
+	if($login == $log) //sprawdzanie czy login jest poprawny
+	{
+		if($haslo == $has) //sprawdzanie czy haslo jest poprawne
+			{
+				echo "Zalogowano<br/>";
+			}
+		else
+			{
+				echo "Złe hasło<br/>";
+			}
+	}
+	else
+		{
+			echo "Zły login<br/>";
+		}
+}
+	var_dump($_POST);
+	mysqli_close($con);
+?>
